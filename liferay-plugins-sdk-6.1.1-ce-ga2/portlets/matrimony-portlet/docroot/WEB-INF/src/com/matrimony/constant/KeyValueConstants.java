73352cd77d8f13fa9145623a8d0a9ca2336d44d2
c.vijayan.mca <c.vijayan.mca@gmail.com>
2012-08-09 18:53:46 +0000

    ALteredfor changes by vijayan

---
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/constant/KeyValueConstants.java
@@ -0,0 +1,27 @@
+package com.matrimony.constant;
+
+public interface KeyValueConstants {
+	String KEY_ID = "keyId";
+	
+	String KEY_VALUE_LIST = "KEY_VALUE_LIST";
+	String ACTION = "action";
+	String ERROR_FETCHING_KEY_VALUE = "Error While fetching KeyValue: ";
+	String ERROR_DELETING_KEY_VALUE = "Error While Deleting KeyValue: ";
+	String ERROR_ADDING_KEY_VALUE = "Error While Adding KeyValue: ";
+	String ERROR_FETCHING_VALUE_LIST = "Error While fetching ValueList: ";
+	String ERROR_FETCHING_KEY_LIST = "Error While fetching KeyList: ";
+	String ERROR_FETCHING_KEY = "Error While fetching Key: ";
+	String ERROR_DELETING_KEY = "Error While Deleting Key: ";
+	String ERROR_ADDING_KEY = "Error While Adding Key: ";
+	String UTF_8 = "UTF-8";
+	String APPLICATION_JSON = "application/json";
+	String DELETE_KEY = "deleteKey";
+	String VALID_KEY = "validKey";
+	String KEY_NAME = "keyName";
+	String ADD_KEY = "addKey";
+	String KEY_VALUE_ID = "keyValueId";
+	String ADD_KEY_VALUE = "addKeyValue";
+	String KEY_VALUE_NAME = "keyValueName";
+	String VALID_KEY_VALUE = "validKeyValue";
+	String DELETE_KEY_VALUE = "deleteKeyValue";
+}
