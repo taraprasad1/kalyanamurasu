89f4fa77b053ca57bc3c63b586b12d0648ccb209
c.vijayan.mca <c.vijayan.mca@gmail.com>
2012-08-09 18:58:21 +0000

    ALteredfor changes by vijayan

---
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/service/base/ProfileLocalServiceClpInvoker.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.matrimony.service.base;
+
+import com.matrimony.service.ProfileLocalServiceUtil;
+
+import java.util.Arrays;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class ProfileLocalServiceClpInvoker {
+	public ProfileLocalServiceClpInvoker() {
+		_methodName0 = "addProfile";
+
+		_methodParameterTypes0 = new String[] { "com.matrimony.model.Profile" };
+
+		_methodName1 = "createProfile";
+
+		_methodParameterTypes1 = new String[] { "long" };
+
+		_methodName2 = "deleteProfile";
+
+		_methodParameterTypes2 = new String[] { "long" };
+
+		_methodName3 = "deleteProfile";
+
+		_methodParameterTypes3 = new String[] { "com.matrimony.model.Profile" };
+
+		_methodName4 = "dynamicQuery";
+
+		_methodParameterTypes4 = new String[] {  };
+
+		_methodName5 = "dynamicQuery";
+
+		_methodParameterTypes5 = new String[] {
+				"com.liferay.portal.kernel.dao.orm.DynamicQuery"
+			};
+
+		_methodName6 = "dynamicQuery";
+
+		_methodParameterTypes6 = new String[] {
+				"com.liferay.portal.kernel.dao.orm.DynamicQuery", "int", "int"
+			};
+
+		_methodName7 = "dynamicQuery";
+
+		_methodParameterTypes7 = new String[] {
+				"com.liferay.portal.kernel.dao.orm.DynamicQuery", "int", "int",
+				"com.liferay.portal.kernel.util.OrderByComparator"
+			};
+
+		_methodName8 = "dynamicQueryCount";
+
+		_methodParameterTypes8 = new String[] {
+				"com.liferay.portal.kernel.dao.orm.DynamicQuery"
+			};
+
+		_methodName9 = "fetchProfile";
+
+		_methodParameterTypes9 = new String[] { "long" };
+
+		_methodName10 = "getProfile";
+
+		_methodParameterTypes10 = new String[] { "long" };
+
+		_methodName11 = "getPersistedModel";
+
+		_methodParameterTypes11 = new String[] { "java.io.Serializable" };
+
+		_methodName12 = "getProfiles";
+
+		_methodParameterTypes12 = new String[] { "int", "int" };
+
+		_methodName13 = "getProfilesCount";
+
+		_methodParameterTypes13 = new String[] {  };
+
+		_methodName14 = "updateProfile";
+
+		_methodParameterTypes14 = new String[] { "com.matrimony.model.Profile" };
+
+		_methodName15 = "updateProfile";
+
+		_methodParameterTypes15 = new String[] {
+				"com.matrimony.model.Profile", "boolean"
+			};
+
+		_methodName50 = "getBeanIdentifier";
+
+		_methodParameterTypes50 = new String[] {  };
+
+		_methodName51 = "setBeanIdentifier";
+
+		_methodParameterTypes51 = new String[] { "java.lang.String" };
+	}
+
+	public Object invokeMethod(String name, String[] parameterTypes,
+		Object[] arguments) throws Throwable {
+		if (_methodName0.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes0, parameterTypes)) {
+			return ProfileLocalServiceUtil.addProfile((com.matrimony.model.Profile)arguments[0]);
+		}
+
+		if (_methodName1.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes1, parameterTypes)) {
+			return ProfileLocalServiceUtil.createProfile(((Long)arguments[0]).longValue());
+		}
+
+		if (_methodName2.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes2, parameterTypes)) {
+			return ProfileLocalServiceUtil.deleteProfile(((Long)arguments[0]).longValue());
+		}
+
+		if (_methodName3.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes3, parameterTypes)) {
+			return ProfileLocalServiceUtil.deleteProfile((com.matrimony.model.Profile)arguments[0]);
+		}
+
+		if (_methodName4.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes4, parameterTypes)) {
+			return ProfileLocalServiceUtil.dynamicQuery();
+		}
+
+		if (_methodName5.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes5, parameterTypes)) {
+			return ProfileLocalServiceUtil.dynamicQuery((com.liferay.portal.kernel.dao.orm.DynamicQuery)arguments[0]);
+		}
+
+		if (_methodName6.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes6, parameterTypes)) {
+			return ProfileLocalServiceUtil.dynamicQuery((com.liferay.portal.kernel.dao.orm.DynamicQuery)arguments[0],
+				((Integer)arguments[1]).intValue(),
+				((Integer)arguments[2]).intValue());
+		}
+
+		if (_methodName7.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes7, parameterTypes)) {
+			return ProfileLocalServiceUtil.dynamicQuery((com.liferay.portal.kernel.dao.orm.DynamicQuery)arguments[0],
+				((Integer)arguments[1]).intValue(),
+				((Integer)arguments[2]).intValue(),
+				(com.liferay.portal.kernel.util.OrderByComparator)arguments[3]);
+		}
+
+		if (_methodName8.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes8, parameterTypes)) {
+			return ProfileLocalServiceUtil.dynamicQueryCount((com.liferay.portal.kernel.dao.orm.DynamicQuery)arguments[0]);
+		}
+
+		if (_methodName9.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes9, parameterTypes)) {
+			return ProfileLocalServiceUtil.fetchProfile(((Long)arguments[0]).longValue());
+		}
+
+		if (_methodName10.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes10, parameterTypes)) {
+			return ProfileLocalServiceUtil.getProfile(((Long)arguments[0]).longValue());
+		}
+
+		if (_methodName11.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes11, parameterTypes)) {
+			return ProfileLocalServiceUtil.getPersistedModel((java.io.Serializable)arguments[0]);
+		}
+
+		if (_methodName12.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes12, parameterTypes)) {
+			return ProfileLocalServiceUtil.getProfiles(((Integer)arguments[0]).intValue(),
+				((Integer)arguments[1]).intValue());
+		}
+
+		if (_methodName13.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes13, parameterTypes)) {
+			return ProfileLocalServiceUtil.getProfilesCount();
+		}
+
+		if (_methodName14.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes14, parameterTypes)) {
+			return ProfileLocalServiceUtil.updateProfile((com.matrimony.model.Profile)arguments[0]);
+		}
+
+		if (_methodName15.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes15, parameterTypes)) {
+			return ProfileLocalServiceUtil.updateProfile((com.matrimony.model.Profile)arguments[0],
+				((Boolean)arguments[1]).booleanValue());
+		}
+
+		if (_methodName50.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes50, parameterTypes)) {
+			return ProfileLocalServiceUtil.getBeanIdentifier();
+		}
+
+		if (_methodName51.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes51, parameterTypes)) {
+			ProfileLocalServiceUtil.setBeanIdentifier((java.lang.String)arguments[0]);
+		}
+
+		throw new UnsupportedOperationException();
+	}
+
+	private String _methodName0;
+	private String[] _methodParameterTypes0;
+	private String _methodName1;
+	private String[] _methodParameterTypes1;
+	private String _methodName2;
+	private String[] _methodParameterTypes2;
+	private String _methodName3;
+	private String[] _methodParameterTypes3;
+	private String _methodName4;
+	private String[] _methodParameterTypes4;
+	private String _methodName5;
+	private String[] _methodParameterTypes5;
+	private String _methodName6;
+	private String[] _methodParameterTypes6;
+	private String _methodName7;
+	private String[] _methodParameterTypes7;
+	private String _methodName8;
+	private String[] _methodParameterTypes8;
+	private String _methodName9;
+	private String[] _methodParameterTypes9;
+	private String _methodName10;
+	private String[] _methodParameterTypes10;
+	private String _methodName11;
+	private String[] _methodParameterTypes11;
+	private String _methodName12;
+	private String[] _methodParameterTypes12;
+	private String _methodName13;
+	private String[] _methodParameterTypes13;
+	private String _methodName14;
+	private String[] _methodParameterTypes14;
+	private String _methodName15;
+	private String[] _methodParameterTypes15;
+	private String _methodName50;
+	private String[] _methodParameterTypes50;
+	private String _methodName51;
+	private String[] _methodParameterTypes51;
+}
