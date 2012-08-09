e64fb0acf6311d32292f661c948895a1a61bbc95
c.vijayan.mca@gmail.com <c.vijayan.mca@gmail.com@b84bf3c5-b111-7ea7-28b8-7b18260d67f5>
2012-08-09 18:57:33 +0000

    ALteredfor changes by vijayan

---
+++ trunk/liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/sql/indexes.sql
@@ -0,0 +1,7 @@
+create index IX_48F621A9 on matrimony_key (name);
+
+create index IX_21AE162E on matrimony_key_value (keyId);
+create index IX_E424368F on matrimony_key_value (name, keyId);
+
+create index IX_C4ADE11B on matrimony_profile (createdBy);
+create index IX_996B8A9A on matrimony_profile (status);
