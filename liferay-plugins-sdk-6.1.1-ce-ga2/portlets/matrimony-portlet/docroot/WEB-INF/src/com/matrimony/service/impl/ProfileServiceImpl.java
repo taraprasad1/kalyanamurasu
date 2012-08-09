204a29ff239e7ae317a8bbff43860e568e9308db
c.vijayan.mca <c.vijayan.mca@gmail.com>
2012-08-09 18:55:08 +0000

    ALteredfor changes by vijayan

---
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.matrimony.service.impl;
+
+import com.matrimony.service.base.ProfileServiceBaseImpl;
+
+/**
+ * The implementation of the profile remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.matrimony.service.ProfileService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author matrimony
+ * @see com.matrimony.service.base.ProfileServiceBaseImpl
+ * @see com.matrimony.service.ProfileServiceUtil
+ */
+public class ProfileServiceImpl extends ProfileServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.matrimony.service.ProfileServiceUtil} to access the profile remote service.
+	 */
+}
