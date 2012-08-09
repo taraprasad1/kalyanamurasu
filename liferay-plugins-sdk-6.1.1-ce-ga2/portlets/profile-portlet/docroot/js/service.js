caeb45a0ad78d720dca1c60c6a6d94ae4bb829e4
c.vijayan.mca <c.vijayan.mca@gmail.com>
2012-08-09 18:06:28 +0000

    ALteredfor changes by vijayan

--- liferay-plugins-sdk-6.1.1-ce-ga2/portlets/profile-portlet/docroot/js/service.js
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/profile-portlet/docroot/js/service.js
@@ -1,8 +1,1 @@
-Liferay.Service.register("Liferay.Service.profile", "com.vijayan.profile.service", "profile-portlet");
-
-Liferay.Service.registerClass(
-	Liferay.Service.profile, "KeyValue",
-	{
-		valueSearch: true
-	}
-);
+Liferay.Service.register("Liferay.Service.profile", "com.matrimony.profile.service", "profile-portlet");
