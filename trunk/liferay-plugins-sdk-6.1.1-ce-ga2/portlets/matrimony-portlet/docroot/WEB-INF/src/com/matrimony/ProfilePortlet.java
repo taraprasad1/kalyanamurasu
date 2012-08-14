c47dbd93d1c03f2a7c9327125cfdffb65c77a3c4
c.vijayan.mca@gmail.com <c.vijayan.mca@gmail.com@b84bf3c5-b111-7ea7-28b8-7b18260d67f5>
2012-08-14 13:56:12 +0000

    ALteredfor changes by vijayan

--- trunk/liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/ProfilePortlet.java
+++ trunk/liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/ProfilePortlet.java
@@ -9,23 +9,26 @@ import javax.portlet.PortletException;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.util.bridges.mvc.MVCPortlet;
+import com.matrimony.constant.ProfileConstants;
 import com.matrimony.model.Profile;
 import com.matrimony.service.ProfileLocalServiceUtil;
+import com.matrimony.util.ProfileUtil;
 
 /**
  * Portlet implementation class Profile
  */
-public class ProfilePortlet extends MVCPortlet {
-
+public class ProfilePortlet extends MatrimonyController {
+	private static final Log LOGGER = LogFactoryUtil.getLog(ProfilePortlet.class);
 	public void deleteAccount(ActionRequest actionRequest,
 			ActionResponse actionResponse) throws IOException, PortletException {
 		
-		long profileId  = ParamUtil.getLong(actionRequest, "profileId");
+		long profileId  = ParamUtil.getLong(actionRequest, ProfileConstants.PROFILE_ID);
 		if(Validator.isNotNull(profileId)) {			
 			try {
 				ProfileLocalServiceUtil.deleteProfile(profileId);
@@ -36,9 +39,9 @@ public class ProfilePortlet extends MVCPortlet {
 			}
 		}
 	}
+
 	public void updateAccount(ActionRequest actionRequest,
 			ActionResponse actionResponse) throws IOException, PortletException {
-		System.out.println("update begin------->");
 		long profileId = ParamUtil.getLong(actionRequest, "profileId");
 		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
 		Profile profile = ProfileLocalServiceUtil.createProfileObj();
@@ -65,62 +68,15 @@ public class ProfilePortlet extends MVCPortlet {
 			 try {
 				ProfileLocalServiceUtil.updateProfile(profile);
 			} catch (SystemException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else {
 			try {
+				profile.setProfileCode(ProfileUtil.getProfileCode(profile));
 				ProfileLocalServiceUtil.addProfile(profile);
 			} catch (SystemException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-
-		System.out.println("update end --------->");
-	}
-	
-	public Profile getAssetFromForm(Profile profile, ActionRequest actionRequest) {
-		
-		profile.setCreatedForMy(ParamUtil.getString(actionRequest, "createdForMy"));
-		profile.setName(ParamUtil.getString(actionRequest, "name"));
-		profile.setGender(ParamUtil.getBoolean(actionRequest, "gender"));
-		profile.setRasi(ParamUtil.getString(actionRequest, "rasi"));
-		profile.setStar(ParamUtil.getString(actionRequest, "star"));
-		profile.setDosam(ParamUtil.getString(actionRequest, "dosam"));
-		profile.setReligion(ParamUtil.getString(actionRequest, "religion"));
-		profile.setCaste(ParamUtil.getString(actionRequest, "caste"));
-		profile.setSubCaste(ParamUtil.getString(actionRequest, "subCaste"));
-		profile.setHeight(ParamUtil.getString(actionRequest, "height"));
-		profile.setWeight(ParamUtil.getString(actionRequest, "weight"));
-		profile.setComplexion(ParamUtil.getString(actionRequest, "complexion"));
-		profile.setMotherTongue(ParamUtil.getString(actionRequest, "motherTongue"));
-		profile.setLanguageKnown(ParamUtil.getString(actionRequest, "languageKnown"));
-		profile.setMaritalStatus(ParamUtil.getString(actionRequest, "maritalStatus"));
-		profile.setChildren(ParamUtil.getInteger(actionRequest, "children"));
-		profile.setCountry(ParamUtil.getString(actionRequest, "country"));
-		profile.setState(ParamUtil.getString(actionRequest, "state"));
-		profile.setCity(ParamUtil.getString(actionRequest, "city"));
-		profile.setAddress(ParamUtil.getString(actionRequest, "address"));
-		profile.setPinCode(ParamUtil.getString(actionRequest, "pinCode"));
-		profile.setPhone(ParamUtil.getString(actionRequest, "phone"));
-		profile.setMobile(ParamUtil.getString(actionRequest, "mobile"));
-		profile.setEmailAddress(ParamUtil.getString(actionRequest, "emailAddress"));
-		profile.setEducation(ParamUtil.getString(actionRequest, "education"));
-		profile.setProfession(ParamUtil.getString(actionRequest, "profession"));
-		profile.setCurrency(ParamUtil.getString(actionRequest, "currency"));
-		profile.setAnnualIncome(ParamUtil.getString(actionRequest, "annualIncome"));
-		profile.setHobbies(ParamUtil.getString(actionRequest, "hobbies"));
-		profile.setAboutMe(ParamUtil.getString(actionRequest, "aboutMe"));
-		profile.setFamilyValue(ParamUtil.getString(actionRequest, "familyValue"));
-		profile.setFamilyType(ParamUtil.getString(actionRequest, "familyType"));
-		profile.setFamilyStatus(ParamUtil.getString(actionRequest, "familyStatus"));
-		profile.setSecurityCode(ParamUtil.getString(actionRequest, "securityCode"));
-		profile.setPhotoSecurityCode(ParamUtil.getString(actionRequest, "photoSecurityCode"));
-		profile.setStatus(ParamUtil.getInteger(actionRequest, "setStatus"));
-		profile.setScheme(ParamUtil.getInteger(actionRequest, "scheme"));
-		profile.setHoroscope(ParamUtil.getBoolean(actionRequest, "horoscope"));
-		
-		return profile;
 	}
 }
