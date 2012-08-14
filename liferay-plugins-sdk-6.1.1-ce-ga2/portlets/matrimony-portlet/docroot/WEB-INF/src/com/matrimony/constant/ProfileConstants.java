2ffa12c0b8e53842d0cebcf6f64134b47364519e
c.vijayan.mca <c.vijayan.mca@gmail.com>
2012-08-14 13:51:48 +0000

    ALteredfor changes by vijayan

---
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/constant/ProfileConstants.java
@@ -0,0 +1,6 @@
+package com.matrimony.constant;
+
+public interface ProfileConstants {
+	String PROFILE_ID = "profileId";
+	String PROFILE_CODE = "profileCode";
+}
--- liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/util/ProfileUtil.java
+++ liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/util/ProfileUtil.java
@@ -1,12 +1,17 @@
 package com.matrimony.util;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
 import com.matrimony.model.Key;
 import com.matrimony.model.KeyValue;
+import com.matrimony.model.Profile;
 import com.matrimony.service.KeyLocalServiceUtil;
 import com.matrimony.service.KeyValueLocalServiceUtil;
 
@@ -14,14 +19,49 @@ public class ProfileUtil {
 	public static List<KeyValue> getKeyValueList(String keyName)
 	{
 		List<KeyValue> keyValueList = new ArrayList<KeyValue>();
-		Key  key = null;
+		Key key = null;
 		try {
 			key = KeyLocalServiceUtil.keySearch(keyName);
-			keyValueList = KeyValueLocalServiceUtil.valueSearch(key.getKeyId());
+			if (Validator.isNotNull(key)) {
+				keyValueList = KeyValueLocalServiceUtil.valueSearch(key.getKeyId());
+			}
+		} catch (SystemException e) {
+
+		}	
+		return keyValueList;
+	}
+	
+	public static String getProfileCode(Profile profile) {
+		String idCode = "MA";
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int date = cal.get(Calendar.DATE);
+		boolean male = profile.getGender();
+		StringBuilder profileCode = new StringBuilder();	
+		if(male){
+			profileCode.append("M");
+		} else {
+			profileCode.append("F");
+		}
+		long autoId = 0l;
+		try {
+			autoId = CounterLocalServiceUtil.increment(Profile.class.getName());
 		} catch (SystemException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-		return keyValueList;
+		}
+		
+		profileCode.append(autoId);
+		profileCode.append(idCode);
+		String profileName = profile.getName();
+		if (Validator.isNotNull(profileName) && profileName.length() >= 2) {
+			profileName = profileName.substring(0, 2).toUpperCase();
+		}
+		profileCode.append(year);
+		profileCode.append(month);
+		profileCode.append(date);
+		profileCode.append(profileName);
+		return profileCode.toString();
 	}
 }
