056fa629ccbc542bda700796afd5142b2af1f04b
gpravimca@gmail.com <gpravimca@gmail.com@b84bf3c5-b111-7ea7-28b8-7b18260d67f5>
2012-08-14 11:18:37 +0000

    profile configuration developed

---
+++ trunk/liferay-plugins-sdk-6.1.1-ce-ga2/portlets/matrimony-portlet/docroot/WEB-INF/src/com/matrimony/ConfigurationImpl.java
@@ -0,0 +1,31 @@
+package com.matrimony;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portlet.PortletPreferencesFactoryUtil;
+
+public class ConfigurationImpl extends DefaultConfigurationAction {
+	
+	@Override
+	public void processAction(PortletConfig portletConfig,
+			ActionRequest actionRequest, ActionResponse actionResponse)
+			throws Exception {
+		String listView = ParamUtil.getString(actionRequest, "listView");
+		System.out.println("view======>"+ listView);
+		String portletResource = ParamUtil.getString(actionRequest, "portletResource");
+		
+		PortletPreferences preferences = PortletPreferencesFactoryUtil.getPortletSetup(actionRequest, portletResource);
+		if(Validator.isNotNull(preferences)) {
+			preferences.setValue("listView",listView);
+			preferences.store();
+		}
+		super.processAction(portletConfig, actionRequest, actionResponse);
+	}
+
+}
